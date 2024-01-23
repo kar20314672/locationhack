e61f72cbf81029c3f686f123f5a4310ae6f4410c
kar20314672 <148785090+kar20314672@users.noreply.github.com>
2024-01-23 17:53:18 +0530

    kar7.php

--- kar7.php
+++ kar7.php
@@ -1,8 +1,9 @@
 <?php
 $myfile = fopen("testfile.txt", "w");
-$txt = "Latitude".$_GET[""]."Long".$_GET["long"];
+$txt = "Latitude: " . $_GET["lat"] . ", Longitude: " . $_GET["long"];
 fwrite($myfile, $txt);
 fclose($myfile);
 ?>
+
 
 
